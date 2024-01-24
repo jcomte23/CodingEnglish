6f782c72e774a5505b0de3d2880dfc68dd20700b
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-23 19:31:49 -0500

    the web was improveb

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         main: 'index.html',
         'a1_introduction': 'src/a1/introduction.html',
         'a1_numbers': 'src/a1/numbers.html',
+        'dev_webconstruntions': 'src/dev/webconstruction.html',
         // Agrega más páginas según sea necesario
       },
     },
