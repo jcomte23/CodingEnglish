715799f77cbf73ea247580abbd27808b41dc4b41
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-23 12:01:04 -0500

    arreglo en las configuracion del vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         'a1_introduction': 'src/a1/introduction.html',
         'a1_numbers': 'src/a1/numbers.html',
         'dev_webconstruntions': 'src/dev/webconstruction.html',
+        'vocabulary': 'src/vocabulary/home.html',
         // Agrega más páginas según sea necesario
       },
     },
