15c0b3bf7d6cebfdf32b2fb77cd008a87150516c
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-26 07:09:48 -0500

    mejoras en el sistema de traducciones

--- src/js/main.js
+++ src/js/main.js
@@ -25,46 +25,15 @@ function openModal() {
 }
 
 for (let i = 0; i < closeButton.length; i++) {
-
     const elements = closeButton[i];
     elements.onclick = (e) => modalClose();
     modal.style.display = 'none';
-
     window.onclick = function (event) {
         if (event.target == modal) modalClose();
     }
 }
 
-// const btnLangChange = document.querySelector(".lang-toggle")
-// const langMessage = document.querySelector(".lang-message")
-// let lang
 
-// if (localStorage.getItem("lang")) {
-//     lang = localStorage.getItem("lang")
-// } else {
-//     lang = "es"
-// }
-
-// btnLangChange.addEventListener('click', () => {
-//     alert("hola mundo")
-//     if (lang = "es") {
-//         window.changeLanguage('es')
-//     } else {
-//         window.changeLanguage('en')
-//     }
-// })
-
-// switch (lang) {
-//     case "es":
-//         langMessage.textContent = "Español"
-//         break;
-//     case "en":
-//         langMessage.textContent = "English"
-//         break;
-//     default:
-//         langMessage.textContent = "Español"
-//         break;
-// }
 
 
 
