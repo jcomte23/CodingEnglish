95ec0c6573f8a71990c3a19cbac0a995ae639e1c
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-26 07:20:20 -0500

    mejora en la organizacion de carpetas

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,6 @@
 import '../css/style.css'
 import '../css/icons.css'
 
-const modal = document.querySelector('.main-modal');
-const closeButton = document.querySelectorAll('.modal-close');
-
-const btnLang=document.querySelector(".btn-lang")
-
-btnLang.addEventListener("click",() => {
-    openModal()
-})
-
-const modalClose = () => {
-    modal.classList.remove('fadeIn');
-    modal.classList.add('fadeOut');
-    setTimeout(() => {
-        modal.style.display = 'none';
-    }, 500);
-}
-
-function openModal() {
-    modal.classList.remove('fadeOut');
-    modal.classList.add('fadeIn');
-    modal.style.display = 'flex';
-}
-
-for (let i = 0; i < closeButton.length; i++) {
-    const elements = closeButton[i];
-    elements.onclick = (e) => modalClose();
-    modal.style.display = 'none';
-    window.onclick = function (event) {
-        if (event.target == modal) modalClose();
-    }
-}
 
 
 
---
+++ src/js/modalLangs.js
@@ -0,0 +1,34 @@
+import '../css/translate.css'
+import '../css/icons.css'
+
+const modal = document.querySelector('.main-modal');
+const closeButton = document.querySelectorAll('.modal-close');
+
+const btnLang=document.querySelector(".btn-lang")
+
+btnLang.addEventListener("click",() => {
+    openModal()
+})
+
+const modalClose = () => {
+    modal.classList.remove('fadeIn');
+    modal.classList.add('fadeOut');
+    setTimeout(() => {
+        modal.style.display = 'none';
+    }, 500);
+}
+
+function openModal() {
+    modal.classList.remove('fadeOut');
+    modal.classList.add('fadeIn');
+    modal.style.display = 'flex';
+}
+
+for (let i = 0; i < closeButton.length; i++) {
+    const elements = closeButton[i];
+    elements.onclick = (e) => modalClose();
+    modal.style.display = 'none';
+    window.onclick = function (event) {
+        if (event.target == modal) modalClose();
+    }
+}
